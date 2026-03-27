0e1ec5faad4f289e1b0e9f6706bf741b9a71f048
felixtoca10-hub <felixtoca10@gmail.com>
2026-03-27 00:13:58 -0500

    Add files via upload

---
+++ Corte1/C++/Proyecto2/proyecto2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Primera clase: entender "clase" vs "objeto"
+class Persona {
+public:
+	string nombre; //Atributo
+	int edad; //Atributo
+
+	//Método (función dentro de una clase)
+	void saludar(){
+		cout << "Hola, soy " << nombre << " y tengo " << edad << " años.\n";
+	}
+};
+int main() {
+	//Crear un objeto (una " Persona" real)
+	Persona p1;
+	//Asignar valores a los atributos
+	p1.nombre = "Felix";
+	p1.edad = 19;
+	//Llamar un metodo del objeto
+	p1.saludar();
+
+	Persona p2;
+	p2.nombre = "Capi";
+	p2.edad = 18;
+	p2.saludar();
+
+	Persona p3;
+	p3.nombre = "Steven";
+	p3.edad= 18;
+	p3.saludar();
+	return 0;
+}
