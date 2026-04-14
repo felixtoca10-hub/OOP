5703b9c01d8f7e12c8ac41dfdbad2642407c839a
felixtoca10-hub <felixtoca10@gmail.com>
2026-04-14 17:44:45 -0500

    Add files via upload

---
+++ Corte2/C++/Punteros/main40.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    int* v = new int[n];           
+    
+    for (int i = 0; i < n; i++) {
+        
+    }
+    return 0;
+}
