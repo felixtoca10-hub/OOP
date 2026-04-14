a797c10cab665aa8e71982d16e359beac57904be
felixtoca10-hub <felixtoca10@gmail.com>
2026-04-13 20:18:43 -0500

    Add files via upload

---
+++ Corte2/C++/Figura/Main.cpp
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main ( String [] args ) {
+        Figura [] figuras = {
+            new Circulo (5.0) ,
+            new Rectangulo (10.0 , 4.0)
+        };
+
+        for ( Figura f : figuras ) {
+            f . mostrarInfo () ;
+        }
+    }
+}
